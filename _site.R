284906105dfb7b92df0b02ba53ccb32986867caf
eleanorecc <eleanorecampbell@ucsb.edu>
2021-09-22 16:08:33 +0200

    rebuilding site

--- _site.R
+++ _site.R
@@ -8,8 +8,6 @@ suppressPackageStartupMessages({
   library(stringr)
   library(knitr)
   library(httr)
-  library(timevis)
-  library(widgetframe)
   library(geojsonio)
   library(leaflet)
   library(formattable)
@@ -38,35 +36,3 @@ bhiprep_gh_raw <- stringr::str_replace(
   pattern = "github.com",
   replacement = "raw.githubusercontent.com"
 )
-
-# bhi_gh_api <- stringr::str_replace(
-#   bhi_gh,
-#   pattern = "github.com",
-#   replacement = "api.github.com/repos"
-# ) %>% paste(
-#   "git/trees/master?recursive=1", sep = "/"
-# )
-# bhiprep_gh_api <- stringr::str_replace(
-#   bhiprep_gh,
-#   pattern = "github.com",
-#   replacement = "api.github.com/repos"
-# ) %>% paste(
-#   "git/trees/master?recursive=1", sep = "/"
-# )
-
-
-## read in variables if they exist
-scores_csv <- file.path(bhi_gh_raw, "master", "index", "scores.csv")
-layers_csv <- file.path(bhi_gh_raw, "master", "index",  "layers.csv")
-conf_csv <- file.path(bhi_gh_raw, "master", "index", "conf", "goals.csv")
-
-if(RCurl::url.exists(scores_csv)){
-  scores <- readr::read_csv(scores_csv)
-}
-if(RCurl::url.exists(layers_csv)){
-  layers <- readr::read_csv(layers_csv)
-}
-if(RCurl::url.exists(conf_csv)){
-  weight <- readr::read_csv(conf_csv) %>%
-    select(goal, weight)
-}
